bd02b4c18f8e67193ed5079842327ff38159d4c4
dadocding <chiruccidavide@gmail.com>
2026-05-26 21:43:40 +0200

    base della finestra login

---
+++ src/Grafica/LoginFrame.java
@@ -0,0 +1,12 @@
+package Grafica;
+
+import javax.swing.*;
+
+public class LoginFrame {
+    private JPanel frame_login;
+    private JLabel label_titolo;
+    private JTextField textField1;
+    private JPasswordField passwordField1;
+    private JButton accediButton;
+    private JButton registratiButton;
+}
